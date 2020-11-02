cfa5aacb0cbbedc3277537c39edd928b11c9a51c
valdofal <v.dofal@gmail.com>
2020-11-02 14:57:36 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const { empty, withArgs } = require('./eventbox.js')
+const duplicate = require('./streambox')
+// empty()
+// withArgs(['Luffy', 'Zoro', 'Usopp','Robin', 'Nami', 'Sanji', 'Ch0pper', ])
+
+duplicate('Chopin.txt')
